7b147ef4f61640e9fc6163a1814c3de338f4edcb
joaobispo <joaobispo@55f01919-31ce-8f79-5e29-7346526facbf>
2010-04-15 21:33:54 +0000

    [Instruction Block] - Added Partitioner SuperBlock and started MegaBlock; - Added Gatherer class;

---
+++ src/instruction-block/org/ancora/DynamicMapping/Partitioning/Gatherer.java
@@ -0,0 +1,135 @@
+/*
+ *  Copyright 2010 Ancora Research Group.
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  under the License.
+ */
+
+package org.ancora.DynamicMapping.Partitioning;
+
+import org.ancora.DynamicMapping.InstructionBlock.InstructionBlock;
+import org.ancora.DynamicMapping.InstructionBlock.InstructionBlockListener;
+import org.ancora.DynamicMapping.InstructionBlock.InstructionBlockListenerProducer;
+import org.ancora.DynamicMapping.InstructionBlock.InstructionBlockProducer;
+
+/**
+ *
+ * @author Joao Bispo
+ */
+public class Gatherer implements InstructionBlockListenerProducer {
+
+   public Gatherer() {
+      producer = new GathererProducer();
+      //listener = new GathererListener();
+      reset();
+
+   }
+
+   /*
+   public InstructionBlockListener getListener() {
+      return listener;
+   }
+    */
+
+   public void addListener(InstructionBlockListener listener) {
+      producer.addListener(listener);
+   }
+
+   public void accept(InstructionBlock instructionBlock) {
+      if(currentInstructionBlock == null) {
+         startNewBlock(instructionBlock);
+         return;
+      }
+
+      // Check if the blocks are the same
+      if(instructionBlock.getId() == currentInstructionBlock.getId()) {
+         repetitions++;
+      } else {
+         // If not, complete an InstructionBlock and make this current
+         completeBlock();
+         startNewBlock(instructionBlock);
+      }
+   }
+
+   private void completeBlock() {
+      // Build new InstructionBlock
+      InstructionBlock newBlock = new InstructionBlock(currentInstructionBlock.getInstructions(),
+              repetitions, currentInstructionBlock.getId());
+
+      // Notify producer
+      producer.sendBlock(newBlock);
+      reset();
+   }
+
+   public void flush() {
+      if(currentInstructionBlock != null) {
+         completeBlock();
+      }
+
+      producer.endProcessing();
+   }
+
+   private void startNewBlock(InstructionBlock instructionBlock) {
+      currentInstructionBlock = instructionBlock;
+      repetitions = 1;
+   }
+
+   private void reset() {
+      currentInstructionBlock = null;
+      repetitions = -1;
+   }
+
+   /**
+    * INSTANCE VARIABLES
+    */
+   private InstructionBlock currentInstructionBlock;
+   private int repetitions;
+
+   private GathererProducer producer;
+
+
+
+
+   //private GathererListener listener;
+
+/*
+   class GathererListener implements InstructionBlockListener {
+
+      public void accept(InstructionBlock instructionBlock) {
+         throw new UnsupportedOperationException("Not supported yet.");
+      }
+
+      public void flush() {
+         throw new UnsupportedOperationException("Not supported yet.");
+      }
+
+
+   }
+*/
+
+
+
+   class GathererProducer extends InstructionBlockProducer {
+      public void sendBlock(InstructionBlock block) {
+         noticeListeners(block);
+      }
+
+      public void endProcessing() {
+         flushListeners();
+      }
+   }
+
+
+
+
+}
