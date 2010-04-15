1fbe9622b65680e9c1c88a21f3c8cd66af3411e2
joaobispo <joaobispo@55f01919-31ce-8f79-5e29-7346526facbf>
2010-04-15 15:04:05 +0000

    [MicroBlaze Implementations] - Added class LineReader; - Added classes for MicroBlaze Trace;

---
+++ src/microblaze/org/ancora/MicroBlaze/Trace/TraceUtils.java
@@ -0,0 +1,115 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.ancora.MicroBlaze.Trace;
+
+import static org.ancora.MicroBlaze.Trace.TraceDefinitions.EXTENSION_SEPARATOR;
+import static org.ancora.MicroBlaze.Trace.TraceDefinitions.PROPERTIES_SEPARATOR;
+import static org.ancora.MicroBlaze.Trace.TraceDefinitions.PROPERTIES_EXTENSION;
+import static org.ancora.MicroBlaze.Trace.TraceDefinitions.TRACE_PREFIX;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.logging.Logger;
+import org.ancora.common.IoUtilsAppend;
+import org.ancora.common.LineReader;
+
+/**
+ *
+ * @author Joao Bispo
+ */
+public class TraceUtils {
+
+   public static File getTracePropertiesFile(File traceFile, Enum[] keys) {
+      // Get the filename
+      String tracePropertiesFilename = IoUtilsAppend.removeExtension(traceFile.getName(), EXTENSION_SEPARATOR) + EXTENSION_SEPARATOR + PROPERTIES_EXTENSION;
+
+      // Get the file
+      File tracePropertiesFile = new File(tracePropertiesFilename);
+
+      // Check if it exists
+      if (!tracePropertiesFile.exists()) {
+         boolean success = createTraceProperties(traceFile, tracePropertiesFile, keys);
+         if (!success) {
+            Logger.getLogger(TraceDefinitions.class.getName()).
+                    warning("Could not find a TraceProperties file, and failed to create it.");
+         }
+      }
+
+      return tracePropertiesFile;
+   }
+
+   public static boolean createTraceProperties(File traceFile, File tracePropertiesFile, Enum[] keys) {
+      // Open trace file for reading
+      LineReader lineReader = LineReader.createLineReader(traceFile);
+
+      // Prepare first line
+      String line = lineReader.nextLine();
+
+      // Create new properties object
+      Properties newProps = new Properties();
+
+      int counter = 0;
+      int totalElements = keys.length;
+
+      while(counter < totalElements || line != null) {
+         // If line starts with trace prefix, skip it
+         if(line.startsWith(TRACE_PREFIX)) {
+            line = lineReader.nextLine();
+            continue;
+         }
+
+         // Split line by the property separator
+         String[] propertyLine = line.split(PROPERTIES_SEPARATOR);
+
+         // Check if size equals two
+         if (propertyLine.length != 2) {
+            line = lineReader.nextLine();
+            continue;
+         }
+
+         // We found a possibly key-value pair! Check if it matches any of the keys.
+         for(Enum key : keys) {
+            if(key.name().equals(propertyLine[0].trim())) {
+               // Store pair in properties
+               newProps.setProperty(key.name(), propertyLine[1].trim());
+               counter++;
+               continue;
+            }
+         }
+
+      }
+
+      System.out.println("Properties:");
+      System.out.println(newProps);
+
+      if (counter == totalElements) {
+         // Save file
+         // Write properties file.
+         try {
+            newProps.store(new FileOutputStream(tracePropertiesFile), null);
+         } catch (IOException ex) {
+            Logger.getLogger(TraceUtils.class.getName()).
+                    warning("IOException:"+ex);
+            return false;
+         }
+         return true;
+      } else {
+         return false;
+      }
+   }
+
+    public static String safeGet(Enum key, Properties properties) {
+      String cpiString = properties.getProperty(key.name());
+      if(cpiString == null) {
+         Logger.getLogger(TraceDefinitions.class.getName()).
+             warning("Properties does not have a '"+key+"' value. Returning null.");
+      }
+
+      return cpiString;
+   }
+}
