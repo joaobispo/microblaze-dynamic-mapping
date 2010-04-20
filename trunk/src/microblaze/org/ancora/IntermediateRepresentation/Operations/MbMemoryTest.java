3a7eca9717a83623103fa5e84844333b25eeac70
joaobispo <joaobispo@55f01919-31ce-8f79-5e29-7346526facbf>
2010-04-20 19:31:48 +0000

    [MicroBlaze Implementations] - Added MicroBlaze versions of the objects Operation and Operand; - Added MicroBlaze version of ImmutableTest and MemoryTest; - Added objects which calculate the ILP for two different scenerios;

---
+++ trunk/src/microblaze/org/ancora/IntermediateRepresentation/Operations/MbMemoryTest.java
@@ -0,0 +1,57 @@
+/*
+ *  Copyright 2010 Ancora Research Group.
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  under the License.
+ */
+
+package org.ancora.IntermediateRepresentation.Operations;
+
+import org.ancora.IntermediateRepresentation.MemoryTest;
+import org.ancora.IntermediateRepresentation.Operation;
+import org.ancora.MicroBlaze.InstructionName;
+import org.ancora.MicroBlaze.InstructionProperties;
+
+/**
+ *
+ * @author Joao Bispo
+ */
+public class MbMemoryTest implements MemoryTest {
+
+   public boolean isMemoryOperation(Operation operation) {
+      return isLoad(operation) || isStore(operation);
+   }
+
+   public boolean isLoad(Operation operation) {
+      // Check if is MicroBlaze operation
+      if(operation.getType() == MbOperationType.MicroBlazeOperation) {
+         InstructionName mbOp = ((MbOperation) operation).getMbType();
+         return InstructionProperties.LOAD_INSTRUCTIONS.contains(mbOp);
+      } else {
+         return irMemoryTest.isLoad(operation);
+      }
+   }
+
+   public boolean isStore(Operation operation) {
+      // Check if is MicroBlaze operation
+      if(operation.getType() == MbOperationType.MicroBlazeOperation) {
+         InstructionName mbOp = ((MbOperation) operation).getMbType();
+         return InstructionProperties.STORE_INSTRUCTIONS.contains(mbOp);
+      } else {
+         return irMemoryTest.isStore(operation);
+      }
+   }
+
+   private static MemoryTest irMemoryTest = new IrMemoryTest();
+
+}
---
+++ trunk/src/microblaze/org/ancora/Transformations/GeneralMbTransformations.java
@@ -0,0 +1,52 @@
+/*
+ *  Copyright 2010 Ancora Research Group.
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  under the License.
+ */
+
+package org.ancora.Transformations;
+
+import java.util.List;
+import org.ancora.IntermediateRepresentation.Operand;
+import org.ancora.IntermediateRepresentation.Operands.MbOperandType;
+import org.ancora.IntermediateRepresentation.Operation;
+import org.ancora.common.Enums;
+
+/**
+ *
+ * @author Joao Bispo
+ */
+public class GeneralMbTransformations {
+
+   public static List<Operation> transformRegister0(List<Operation> operations) {
+      for(Operation operation : operations) {
+         transformRegister0(operation.getInputs());
+         transformRegister0(operation.getOutputs());
+      }
+
+      return operations;
+   }
+
+   private static void transformRegister0(List<Operand> operands) {
+      for(int i=0; i<operands.size(); i++) {
+         // Check if operand is a MbOperand
+         Operand operand = operands.get(i);
+         if(operand.getType() == MbOperandType.register) {
+            System.out.println("True:"+operand);
+         }
+         
+      }
+
+   }
+}
