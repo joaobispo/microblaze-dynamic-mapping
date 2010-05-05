6b368afa4ac3c0e8e39fa34fa15fe594f046db39
joaobispo <joaobispo@55f01919-31ce-8f79-5e29-7346526facbf>
2010-05-05 23:59:39 +0000

    [MicroBlaze Implementations] - Changed MbTraceReader so it does not need to create Properties files to access the properties from a trace. - Changed way how MbIlpScene2 calculates live-ins and live-outs. Now assumes incoming operations are pure-ir. - Added transformation TransformRegistersToInternalData - Added transformations SingleStaticAssignment and RemoveInternalLoads - [bug] RemoveInternalLoads is removing loads which it shouldn't. It is also assigning operands where it should not.

---
+++ trunk/src/microblaze/org/ancora/Transformations/MicroblazeGeneral/TransformRegistersToInternalData.java
@@ -0,0 +1,68 @@
+/*
+ *  Copyright 2010 Ancora Research Group.
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  under the License.
+ */
+
+package org.ancora.Transformations.MicroblazeGeneral;
+
+import java.util.List;
+import org.ancora.IntermediateRepresentation.Operand;
+import org.ancora.IntermediateRepresentation.Operands.InternalData;
+import org.ancora.IntermediateRepresentation.Operands.MbRegister;
+import org.ancora.IntermediateRepresentation.Operands.MicroblazeType;
+import org.ancora.IntermediateRepresentation.Operation;
+import org.ancora.Transformations.Transformation;
+
+/**
+ *
+ * @author Joao Bispo
+ */
+public class TransformRegistersToInternalData implements Transformation {
+
+   public List<Operation> transform(List<Operation> operations) {
+      for(int i=0; i<operations.size(); i++) {
+         Operation operation = operations.get(i);
+
+         List<Operand> inputs = operation.getInputs();
+         for(int j = 0; j<inputs.size(); j++) {
+            Operand input = inputs.get(j);
+
+            if(input.getType() != MicroblazeType.MbRegister) {
+               continue;
+            }
+
+            String registerName = input.toString();
+            Operand newOperand = new InternalData(registerName, input.getBits());
+            inputs.set(j, newOperand);
+         }
+
+         List<Operand> outputs = operation.getOutputs();
+         for(int j = 0; j<outputs.size(); j++) {
+            Operand output = outputs.get(j);
+
+            if(output.getType() != MicroblazeType.MbRegister) {
+               continue;
+            }
+
+            String registerName = output.toString();
+            Operand newOperand = new InternalData(registerName, output.getBits());
+            outputs.set(j, newOperand);
+         }
+      }
+
+      return operations;
+   }
+
+}
